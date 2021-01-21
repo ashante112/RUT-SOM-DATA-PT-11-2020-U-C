57ec0b6fa600e0d8f97ba818760dc244beb06633
Swati D <60013603+swati-dontamsetti@users.noreply.github.com>
2021-01-20 21:42:25 -0500

    solutions, summary, pdf

---
+++ Class_files/06-Python-APIs/3/06-Evr_Jupyter_Gmaps/Solved/config.py
@@ -0,0 +1,2 @@
+# Enter your API key
+gkey = "AIzaSyBHm7y412syNuC-aK_3dSUuHm5VQsg7BzE"
---
+++ Class_files/06-Python-APIs/3/09-Stu_Census_API_Gmap/Solved/config.py
@@ -0,0 +1,5 @@
+# Enter your API keys
+
+api_key = "c979fccd3a7ca61107c3fbc24c1f810f15daeb24"
+
+gkey = "AIzaSyBHm7y412syNuC-aK_3dSUuHm5VQsg7BzE"
